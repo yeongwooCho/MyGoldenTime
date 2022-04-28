18b203e086debc1f2e7dc212bd78a0b10222bf13
yeongwoo_Cho <jyy0223@naver.com>
2022-04-28 12:22:42 +0900

    Initial Commit

---
+++ MyGoldenTime/MyGoldenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGoldenTimeApp.swift
+//  MyGoldenTime
+//
+//  Created by yeongwoocho on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct MyGoldenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
