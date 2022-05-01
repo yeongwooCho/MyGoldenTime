acf4e1f4ed2c7cafc1c67e6495ea727e435d00c9
yeongwoo_Cho <jyy0223@naver.com>
2022-05-01 23:39:27 +0900

    add tab view

---
+++ MyGoldenTime/TabView.swift
@@ -0,0 +1,21 @@
+//
+//  TabView.swift
+//  MyGoldenTime
+//
+//  Created by yeongwoocho on 2022/05/01.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView()
+    }
+}
