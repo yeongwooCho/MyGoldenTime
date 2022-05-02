8ec17b25ce61b1892419a5e88585bb70d79d8ced
yeongwoo_Cho <jyy0223@naver.com>
2022-05-02 09:25:15 +0900

    add InputStateView file

---
+++ MyGoldenTime/InputStateView.swift
@@ -0,0 +1,20 @@
+//
+//  InputStateView.swift
+//  MyGoldenTime
+//
+//  Created by yeongwoocho on 2022/05/02.
+//
+
+import SwiftUI
+
+struct InputStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputStateView()
+    }
+}
