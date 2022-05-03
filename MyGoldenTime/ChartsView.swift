7e9190983da4b0ca2bd3636425db666811a9dc25
yeongwoo_Cho <jyy0223@naver.com>
2022-05-03 12:38:04 +0900

    add ChartsView file

---
+++ MyGoldenTime/ChartsView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartsView.swift
+//  MyGoldenTime
+//
+//  Created by yeongwoocho on 2022/05/03.
+//
+
+import SwiftUI
+
+struct ChartsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartsView()
+    }
+}
