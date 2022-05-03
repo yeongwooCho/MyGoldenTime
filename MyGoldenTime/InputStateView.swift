9ba93ed1fefee058e87c972e14633a7d937eddea
yeongwoo_Cho <jyy0223@naver.com>
2022-05-03 09:23:54 +0900

    add InputStateView UI

--- MyGoldenTime/InputStateView.swift
+++ MyGoldenTime/InputStateView.swift
@@ -8,38 +8,76 @@
 import SwiftUI
 
 struct InputStateView: View {
+    
+    @State private var inputWhatTodo = ""
+    
+    @State private var inputWhyTodo = ""
+    
+    @State private var seletedEnergyState = 3
+    
+    @State private var seletedConcentrationState = 3
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("!23")
- 
-                VStack {
-                    Text("asdf")
-                    Text("asdf")
+        GeometryReader { proxy in
+            NavigationView {
+                VStack(alignment: .center, spacing: 20.0) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Spacer()
+                        Text("당신은 지금 무엇을 하고 있나요?")
+                            .fontWeight(.bold)
+                        TextField("10자 이내로 입력해주세요.", text: $inputWhatTodo)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Spacer()
+                        Text("왜 그것을 하고 있나요?")
+                            .fontWeight(.bold)
+                        TextField("10자 이내로 입력해주세요.", text: $inputWhyTodo)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Spacer()
+                    } // VStack
+                    .padding(20)
+                    .frame(width: proxy.size.width - 40, height: 200)
+                    .background()
+                    .cornerRadius(20)
+                    .shadow(color: .gray, radius: 10, x: 5, y: 5)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        HStack(spacing: 20) {
+                            Text("에너지 상태")
+                                .fontWeight(.bold)
+                            Picker("당신의 상태를 선택하세요", selection: $seletedEnergyState) {
+                                ForEach(0...5, id: \.self) { Text("\($0)") }
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                        } // HStack
+                        .padding(.horizontal, 20)
+                        
+                        HStack(spacing: 20) {
+                            Text("집중력 상태")
+                                .fontWeight(.bold)
+                            Picker("당신의 상태를 선택하세요", selection: $seletedConcentrationState) {
+                                ForEach(0...5, id: \.self) { Text("\($0)") }
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                        } // HStack
+                        .padding(.horizontal, 20)
+                    } // VStack
+                    .frame(width: proxy.size.width - 40, height: 140)
+                    .background()
+                    .cornerRadius(20)
+                    .shadow(color: .gray, radius: 10, x: 5, y: 5)
                 } // VStack
-                
-                VStack {
-                    HStack {
-                        Text("에너지 상태")
-                        Text("asdf")
-                    } // HStack
-                    HStack {
-                        Text("집중력 상태")
-                        Text("asdf")
-                    } // HStack
-                } // VStack
-            } // VStack
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: Text("설정")) {
-                        Image(systemName: "gear")
-                            .font(.system(size: 25))
-                            .foregroundColor(.black)
-
+                .navigationTitle("상태 입력")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: Text("설정")) {
+                            Image(systemName: "gear")
+                                .font(.system(size: 25))
+                                .foregroundColor(.black)
+                        }
                     }
                 }
-            }
-        } // NavigationView
+            } // NavigationView
+        } // GeometryReader
     }
 }
 
