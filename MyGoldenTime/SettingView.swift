123b20e0b510e2c16d20782ece2de415f73f6a94
yeongwoo_Cho <jyy0223@naver.com>
2022-05-03 10:49:45 +0900

    add SettingView file

---
+++ MyGoldenTime/SettingView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingView.swift
+//  MyGoldenTime
+//
+//  Created by yeongwoocho on 2022/05/03.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Hello world")
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
