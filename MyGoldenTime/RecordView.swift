ecd161a50a7093039c6f227deaa50735b4fe08c5
yeongwoo_Cho <jyy0223@naver.com>
2022-05-05 16:17:06 +0900

    add: RecordView file

---
+++ MyGoldenTime/RecordView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordView.swift
+//  MyGoldenTime
+//
+//  Created by yeongwoocho on 2022/05/05.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView()
+    }
+}
